3060475a9c6363103eea3bfc9c9e88aaf8a03285
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-01-31 10:20:15 +0100

    Create BooleanExtension.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To C#/BooleanExtension.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+namespace BloodOfEvil.Extensions
+  {
+  public static class BooleanExtension
+  {
+      /// <summary>
+      /// Inverse la valeur duCe s boolean.
+      /// </summary>
+      public static void Inverse(this bool boolean)
+      {
+          boolean = !boolean;
+      }
+  }
+}
