968d2f95bbb8d55fbf2a4b4b786c0e50a913c872
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-01-31 12:26:06 +0100

    Update SingletonManager.cs

--- Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/SingletonManager.cs
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/SingletonManager.cs
@@ -1,70 +1,62 @@
 using UnityEngine;
 using System.Collections;
-using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 namespace BloodOfEvil.Utilities
 {
     /// <summary>
-    /// Gère les singletons de type MonoBehaviour et leurs différentes érreurs potentielles.
+    /// Cette classe permet de relancer votre application sans avoir des problèmes d'instances, d'initialisation ou d'évênements sur vos singletons de type de données ou héritant de MonoBehaviour.
+    /// Elle doit se situer dans votre scène de base et ne dois jamais être détruite à part au moment où vous relancez votre application.
     /// </summary>
-    public class ASingletonMonoBehaviour<TSingletonType> : MonoBehaviour
-        where TSingletonType : ASingletonMonoBehaviour<TSingletonType>
+    public class SingletonManager : ASingletonMonoBehaviour<SingletonManager>
     {
         #region Fields
-        private static TSingletonType instance;
-        private static bool haveBeenInitialized;
+        private const string defaultSceneName = "Persistent";
         #endregion
 
-        #region Properties
-        public static TSingletonType Instance
+        #region Unity Behaviour
+        /// <summary>
+        /// Initialize tous les singletons de la scène de base.
+        /// </summary>
+        private void Awake()
         {
-            get
-            {
-                if (null == instance)
-                {
-                    instance = (TSingletonType)FindObjectOfType(typeof(TSingletonType));
+            //var d = DataAndDifficultyController.Instance;
+    //
+            //var e = LanguageService.Instance;
+            var f = SingletonManager.Instance;
 
-                    if (FindObjectsOfType(typeof(TSingletonType)).Length > 1)
-                    {
-                        Debug.LogErrorFormat("[ASingletonMonoBehaviour] : le singleton de type {0} éxiste en plusieurs éxemplaires ce qui n'est pas logique.",
-                            typeof(TSingletonType).Name);
-
-                        return instance;
-                    }
-
-                    else if (instance == null)
-                        Debug.LogErrorFormat("[ASingletonMonoBehaviour] : le singleton de type {0} a une instance de valeur null.",
-                            typeof(TSingletonType).Name);
-
-                    else
-                        instance.Initialize();
-                }
-
-                return instance;
-            }
+            DontDestroyOnLoad(gameObject);
         }
         #endregion
 
-        #region Virtual Behaviour
+        #region Public Behaviour
         /// <summary>
-        /// La méthode d'initialisation de notre singleton.
+        /// Cette méthode est appelé lorsque l'on doit relancer notre jeu.
         /// </summary>
-        public virtual void Initialize()
+        public void RestartApplication()
         {
-            if (haveBeenInitialized)
-                Debug.LogErrorFormat("[ASingletonMonoBehaviour] : le singleton de type {0} a une instance déjà initialisée.",
-                    typeof(TSingletonType).Name);
+            //// Réinitialise tous les singletons héritant de MonoBehaviour en leur disant qu'ils peuvent être de nouveau initializer et en désabonnant tous leurs évênements.
+            //GameController.Instance.Reinitialize();
+            //ScoreModule.Instance.Reinitialize();
+            //ScoreInterfaceModule.Instance.Reinitialize();
+            //DataAndDifficultyController.Instance.Reinitialize();
+            //
+            //// Réinitialise tous les singletons héritant de données héritant d'aucune classe en leur disant qu'ils peuvent être de nouveau initializer et en désabonnant tous leurs évênements.
+            //LanguageService.Instance.Reinitialize();
+    //
+            //// Détruit tous les singletons héritant de MonoBehaviour.
+            //Destroy(GameController.Instance.gameObject);
+            //Destroy(ScoreModule.Instance.gameObject);
+            //Destroy(ScoreInterfaceModule.Instance.gameObject);
+            //Destroy(DataAndDifficultyController.Instance.gameObject);
 
-            haveBeenInitialized = true;
-        }
+            // Charge la scène par défault.
+            SceneManager.LoadScene(defaultSceneName);
 
-        /// <summary>
-        /// Cette méthode est appelé lorsque l'on relance l'application.
-        /// Les enfants de cette classe devront l'override et réinitialiser tout leurs évênements (= null).
-        /// </summary>
-        public virtual void Reinitialize()
-        {
-            haveBeenInitialized = false;
+            // S'auto-détruit.
+            base.Reinitialize();
+            Destroy(gameObject);
         }
         #endregion
     }
