8c8e38edbc585a03486d93d27eca8c8cc7d9b64b
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-02-17 11:25:17 +0100

    Create ImagesService.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Scene/Services/ImagesService.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImagesService : ASingletonMonoBehaviour<ImagesService>
+{
+    #region Public Behaviour
+    /// <summary>
+    /// Fait clignotter une image pendant ("timerPerBlink" * numberOfBlinks * 2).
+    /// </summary>
+    public IEnumerator Blink(object[] parameters)
+    {
+        Image image = (Image)parameters[0];
+        Sprite blinkSprite = (Sprite)parameters[1];
+        float timePerBlink = (float)parameters[2];
+        int numberOfBlinks = (int)parameters[3];
+
+        Sprite defaultSprite = image.sprite;
+
+        yield return null;
+
+        for (int i = 0; i < numberOfBlinks; i++)
+        {
+            image.sprite = blinkSprite;
+
+            yield return new WaitForSeconds(timePerBlink);
+
+            image.sprite = defaultSprite;
+        }
+    }
+    #endregion
+}
