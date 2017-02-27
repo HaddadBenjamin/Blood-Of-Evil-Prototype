6c9d6fe2d4b61384ee440b954317f16e6c3b166a
Benjamin Haddad <haddad.b.pro@gmail.com>
2017-02-27 20:53:30 +0100

    + Résolution d'un petit problème de compilation de NGTools.

---
+++ Blood Of Evil/Assets/Project/Imported/NGTools/NGRemoteScene/NetworkWrappers/NetGameObjectData.cs
@@ -0,0 +1,45 @@
+﻿namespace NGTools.NGRemoteScene
+{
+	public sealed class NetGameObjectData
+	{
+		public readonly int				gameObjectInstanceID;
+		public readonly string			tag;
+		public readonly int				layer;
+		public readonly bool			isStatic;
+		public readonly NetComponent[]	components;
+
+		public static void	Serialize(ServerGameObject NGGameObject, ByteBuffer buffer)
+		{
+			NGGameObject.ProcessComponents();
+
+			buffer.Append(NGGameObject.instanceID);
+			buffer.AppendUnicodeString(NGGameObject.gameObject.tag);
+			buffer.Append(NGGameObject.gameObject.layer);
+			buffer.Append(NGGameObject.gameObject.isStatic);
+			buffer.Append(NGGameObject.components.Count);
+
+			for (int i = NGGameObject.components.Count - 1; i >= 0; --i)
+				NetComponent.Serialize(NGGameObject.components[i], buffer);
+		}
+
+		public static NetGameObjectData	Deserialize(ByteBuffer buffer)
+		{
+			return new NetGameObjectData(buffer);
+		}
+
+		private	NetGameObjectData(ByteBuffer buffer)
+		{
+			this.gameObjectInstanceID = buffer.ReadInt32();
+			this.tag = buffer.ReadUnicodeString();
+			this.layer = buffer.ReadInt32();
+			this.isStatic = buffer.ReadBoolean();
+
+			int	length = buffer.ReadInt32();
+
+			this.components = new NetComponent[length];
+
+			for (int i = 0; i < length; i++)
+				this.components[i] = NetComponent.Deserialize(buffer);
+		}
+	}
+}
---
+++ Blood Of Evil/Assets/Project/Imported/NGTools/NGRemoteScene/NetworkWrappers/NetMaterial.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace NGTools.NGRemoteScene
+{
+	public sealed class NetMaterial
+	{
+		public readonly int						instanceID;
+		public readonly string					name;
+		public string							shader;
+		public readonly NetMaterialProperty[]	properties;
+
+		public int	selectedShader = -1;
+
+		public static void	Serialize(Material mat, NGShader shader, ByteBuffer buffer)
+		{
+			buffer.Append(mat.GetInstanceID());
+			buffer.AppendUnicodeString(mat.name);
+
+			if (mat.shader != null)
+				buffer.AppendUnicodeString(mat.shader.name);
+			else
+				buffer.Append(0);
+
+			buffer.Append(shader.properties.Length);
+
+			for (int i = 0; i < shader.properties.Length; i++)
+				NetMaterialProperty.Serialize(mat, shader.properties[i], buffer);
+		}
+
+		public static NetMaterial	Deserialize(ByteBuffer buffer)
+		{
+			return new NetMaterial(buffer);
+		}
+
+		private	NetMaterial(ByteBuffer buffer)
+		{
+			this.instanceID = buffer.ReadInt32();
+			this.name = buffer.ReadUnicodeString();
+			this.shader = buffer.ReadUnicodeString();
+			this.properties = new NetMaterialProperty[buffer.ReadInt32()];
+
+			for (int i = 0; i < this.properties.Length; i++)
+				this.properties[i] = NetMaterialProperty.Deserialize(buffer);
+		}
+	}
+}
