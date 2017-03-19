31b435d5019a22c54d68dbc54cdacc99fbf7941e
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 19:28:50 +0100

    Create EditorExtension.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/EditorExtension.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace BloodOfEvil.Extensions
+{
+    public static class EditorExtension
+    {
+        /// <summary>
+        /// Affiche une énumération de type bitmask dans l'inspecteur.
+        /// </summary>
+        public static int DrawBitMaskField(Rect aPosition, int aMask, System.Type aType, GUIContent aLabel)
+        {
+            var itemNames = System.Enum.GetNames(aType);
+            var itemValues = System.Enum.GetValues(aType) as int[];
+
+            int val = aMask;
+            int maskVal = 0;
+
+            for (int i = 0; i < itemValues.Length; i++)
+            {
+                if (itemValues[i] != 0)
+                {
+                    if ((val & itemValues[i]) == itemValues[i])
+                        maskVal |= 1 << i;
+                }
+                else if (val == 0)
+                    maskVal |= 1 << i;
+            }
+
+            int newMaskVal = EditorGUI.MaskField(aPosition, aLabel, maskVal, itemNames);
+            int changes = maskVal ^ newMaskVal;
+
+            for (int i = 0; i < itemValues.Length; i++)
+            {
+                if ((changes & (1 << i)) != 0)            // has this list item changed?
+                {
+                    if ((newMaskVal & (1 << i)) != 0)     // has it been set?
+                    {
+                        if (itemValues[i] == 0)           // special case: if "0" is set, just set the val to 0
+                        {
+                            val = 0;
+                            break;
+                        }
+                        else
+                            val |= itemValues[i];
+                    }
+                    else                                  // it has been reset
+                        val &= ~itemValues[i];
+                }
+            }
+            return val;
+        }
+    }
+}
