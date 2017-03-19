95be9a543997174ab7c4bed68043cee720a9e7c4
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 19:06:01 +0100

    Create ComponentsAndGameObjectsModifyEnableState.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/Behaviours/ComponentsAndGameObjectsModifyEnableState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BloodOfEvil.Utilities
+{
+    /// <summary>
+    /// Permet d'activer ou désactiver certain objets et composants.
+    /// </summary>
+    public class ComponentsAndGameObjectsModifyEnableState : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField, Tooltip("Ce sont les objets à activer lorsque le paramètre envoyé est vrai.")]
+        private GameObject[] objectsToActive;
+        [SerializeField, Tooltip("Ce sont les objets à désactiver lorsque le paramètre envoyé est vrai.")]
+        private GameObject[] objectsToDesactive;
+
+        [SerializeField, Tooltip("Ce sont les composants à activer lorsque le paramètre envoyé est vrai.")]
+        private MonoBehaviour[] componentsToActive;
+        [SerializeField, Tooltip("Ce sont les composants à désactiver lorsque le paramètre envoyé est vrai.")]
+        private MonoBehaviour[] componentsToDesactive;
+        #endregion
+
+        #region Public Behaviour
+        public void ModifyEnableState(bool enableState)
+        {
+            Array.ForEach(this.objectsToActive, objectToActive => objectToActive.SetActive(enableState));
+            Array.ForEach(this.objectsToDesactive, objectToDesactive => objectToDesactive.SetActive(!enableState));
+
+            Array.ForEach(this.componentsToActive, componentToActive => componentToActive.enabled = enableState);
+            Array.ForEach(this.componentsToDesactive, componentToDesactive => componentToDesactive.enabled = !enableState);
+        }
+        #endregion
+    }
+}
