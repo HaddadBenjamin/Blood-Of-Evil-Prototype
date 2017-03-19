471432e93b6254b562e2ee14e277dfcc22038b35
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 18:35:21 +0100

    Create SpriteReferencesService.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Scene/Services/References Service Updated/Sprites/SpriteReferencesService.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Stoque les références sur les sprites de votre application.
+/// </summary>
+public class SpriteReferencesService : AObjectReferencesService<Sprite, PrefabReferencesService>
+{
+}
