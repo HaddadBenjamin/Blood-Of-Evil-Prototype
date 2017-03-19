fe7f9779ed8d555542a1b212419da2c1ed086993
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 18:34:40 +0100

    Create GameObjectReferencesInSceneService.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Scene/Services/References Service Updated/Game Objects/GameObjectReferencesInSceneService.cs
@@ -0,0 +1,11 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stock les références sur les gameobjects de votre scène.
+/// </summary>
+public class GameObjectReferencesInSceneService : AObjectReferencesService<GameObject, GameObjectReferencesInSceneService>
+{
+}
