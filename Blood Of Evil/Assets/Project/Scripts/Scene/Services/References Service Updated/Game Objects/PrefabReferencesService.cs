c66c69970cab879b631d9c6b5a0468a5cdabef46
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 18:35:03 +0100

    Create PrefabReferencesService.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Scene/Services/References Service Updated/Game Objects/PrefabReferencesService.cs
@@ -0,0 +1,11 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stock les références sur les préfabs, on les utilisera potentiellement pour les instancier.
+/// </summary>
+public class PrefabReferencesService : AObjectReferencesService<GameObject, PrefabReferencesService>
+{
+}
