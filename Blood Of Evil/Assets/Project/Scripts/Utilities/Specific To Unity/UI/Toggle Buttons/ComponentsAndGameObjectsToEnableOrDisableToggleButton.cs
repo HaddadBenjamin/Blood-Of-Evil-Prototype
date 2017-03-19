bffee4b6a430459ee458c15f2f49c71153be0cd8
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 18:50:50 +0100

    Create ComponentsAndGameObjectsToEnableOrDisableToggleButton.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/UI/Toggle Buttons/ComponentsAndGameObjectsToEnableOrDisableToggleButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BloodOfEvil.Utilities.UI
+{
+    /// <summary>
+    /// Permet d'activer certain composant ou objets et dans désactiver lorsque l'état de notre bouton type case à coché change.
+    /// </summary>
+    public class ComponentsAndGameObjectsToEnableOrDisableToggleButton : AToggleButtonAction
+    {
+        #region Fields
+        [SerializeField, Tooltip("Ce sont les objets à activer lorsque l'état de notre case à coché est allumé.")]
+        private GameObject[] objectsToActiveOnOnButtonState;
+        [SerializeField, Tooltip("Ce sont les objets à activer lorsque l'état de notre case à coché est éteind.")]
+        private GameObject[] objectsToActiveOnOffButtonState;
+
+        [SerializeField, Tooltip("Ce sont les composants à activer lorsque l'état de notre case à coché est allumé.")]
+        private MonoBehaviour[] componentsToActiveOnOnButtonState;
+        [SerializeField, Tooltip("Ce sont les composants à activer lorsque l'état de notre case à coché est éteind.")]
+        private MonoBehaviour[] componentsToActiveOnOffButtonState;
+        #endregion
+
+        #region Override Behaviour
+        protected override void ButtonToggleAction(bool isOn)
+        {
+            Array.ForEach(this.objectsToActiveOnOnButtonState, objectToActive => objectToActive.SetActive(isOn));
+            Array.ForEach(this.objectsToActiveOnOffButtonState, objectToDesactive => objectToDesactive.SetActive(!isOn));
+
+            Array.ForEach(this.componentsToActiveOnOnButtonState, componentToActive => componentToActive.enabled = isOn);
+            Array.ForEach(this.componentsToActiveOnOffButtonState, componentToDesactive => componentToDesactive.enabled = !isOn);
+        }
+        #endregion
+    }
+}
