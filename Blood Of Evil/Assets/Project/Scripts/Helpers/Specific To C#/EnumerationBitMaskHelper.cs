3a664867d251ac7f6dae6d81f2eddba64d076a38
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 19:24:18 +0100

    Create EnumerationBitMaskHelper.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Helpers/Specific To C#/EnumerationBitMaskHelper.cs
@@ -0,0 +1,33 @@
+namespace BloodOfEvil.Helpers
+{
+    /// <summary>
+    /// Facilite l'utilisation des énumérations de bitmask.
+    /// </summary>
+    public static class EnumerationBitMaskHelper
+    {
+        /// <summary>
+        /// Renvoit si le le bit "bitMaskBit" est actif dans le bitMask "bitMask".
+        /// </summary>
+        public static bool DoesBitMaskBitIsSetted<TBitMaskType>(TBitMaskType bitMask, TBitMaskType bitMaskBit) where TBitMaskType : struct
+        {
+        }
+            return ((int)(object)bitMask & (int)(object)bitMaskBit) != 0;
+        }
+
+        /// <summary>
+        /// Active le bit "bitMaskBit" au bitMask "bitMask".
+        /// </summary>
+        public static void AddBitMaskBit<TBitMaskType>(ref TBitMaskType bitMask, TBitMaskType bitMaskBit) where TBitMaskType : struct
+        {
+            bitMask = (TBitMaskType)(object)((int)(object)bitMask | (int)(object)bitMaskBit);
+        }
+
+        /// <summary>
+        /// Désactive le bit "bitMaskBit" au bitMask "bitMask".
+        /// </summary>
+        public static void RemoveBitMaskBit<TBitMaskType>(ref TBitMaskType bitMask, TBitMaskType bitMaskBit) where TBitMaskType : struct
+        {
+            bitMask = (TBitMaskType)(object)((int)(object)bitMask & (~(int)(object)bitMaskBit));
+        }
+    }
+}
