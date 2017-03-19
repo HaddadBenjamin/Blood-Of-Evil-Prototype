ebb974f77adcef444577f0a3f5ab74626cce1466
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 19:01:55 +0100

    Create EnumerationBitMaskPropertyDrawer.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/Property Drawer/Enum Bit Mask/EnumerationBitMaskPropertyDrawer.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace BloodOfEvil.Utilities
+{
+    /// <summary>
+    /// Affiche une énumération de type bitmask dans l'inspecteur.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(EnumerationBitMaskAttribute))]
+    public class EnumerationBitMaskPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            var propertyType = attribute as EnumerationBitMaskAttribute;
+
+            label.text = label.text + "(" + serializedProperty.intValue + ")";
+            serializedProperty.intValue = EditorExtension.DrawBitMaskField(position, serializedProperty.intValue, propertyType.propertyType, label);
+        }
+    }
+}
