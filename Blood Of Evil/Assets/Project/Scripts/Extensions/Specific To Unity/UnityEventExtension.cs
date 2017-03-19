5b4fee088d01b2b6e0d5c9ab256521fbca3624fa
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 19:36:55 +0100

    Update UnityEventExtension.cs

--- Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/UnityEventExtension.cs
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/UnityEventExtension.cs
@@ -7,20 +7,26 @@ namespace BloodOfEvil.Extensions
 {
     public static class UnityEventExtension
     {
+        /// <summary>
+        /// Appele l'évenement de type boolean avec son paramètre si il éxiste.
+        /// </summary>
         public static void SafeInvoke(this UnityBoolEvent unityEvent, bool parameter)
         {
             if (null != unityEvent)
                 unityEvent.Invoke(parameter);
         }
 
+        /// <summary>
+        /// Appele l'évenement de type float avec son paramètre si il éxiste.
+        /// </summary>
         public static void SafeInvoke(this UnityFloatEvent unityEvent, float parameter)
         {
             if (null != unityEvent)
                 unityEvent.Invoke(parameter);
         }
-        
+
         /// <summary>
-        /// Appelle la méthode de l'unity event si il y en a une.
+        /// Appele l'évenement si il éxiste.
         /// </summary>
         public static void SafeInvoke(this UnityEvent unityEvent)
         {
