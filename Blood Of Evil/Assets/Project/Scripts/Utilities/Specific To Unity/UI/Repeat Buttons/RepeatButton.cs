0fdb283ce435c221d27a40c3eda8e05d9257a1eb
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 18:45:27 +0100

    Create RepeatButton.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/UI/Repeat Buttons/RepeatButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+namespace BloodOfEvil.Utilities.UI
+{
+    /// <summary>
+    /// C'est un bouton dont l'action est appelé à chaque fois que l'on reste appuyé dessus.
+    /// </summary>
+    public class RepeatButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        #region Fields
+        /// <summary>
+        /// Permet de savoir si l'on doit appelé ou non l'action du bouton.
+        /// </summary>
+        private bool canCallRepeatEvent = false;
+        [SerializeField, Tooltip("C'est l'action du bouton à appeler.")]
+        private UnityEvent repeatEvent;
+        #endregion
+
+        #region Interface Behaviour
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+        {
+            this.canCallRepeatEvent = true;
+        }
+
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            this.canCallRepeatEvent = false;
+        }
+        #endregion
+
+        #region Unity Behaviour
+        private void LateUpdate()
+        {
+            if (this.canCallRepeatEvent)
+                this.repeatEvent.SafeInvoke();
+        }
+        #endregion
+    }
+}
