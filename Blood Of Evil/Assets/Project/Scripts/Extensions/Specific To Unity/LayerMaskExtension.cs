9d910445a6378d9f936394c7d9739c703b6944e9
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-19 19:30:45 +0100

    Update LayerMaskExtension.cs

--- Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/LayerMaskExtension.cs
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/LayerMaskExtension.cs
@@ -3,24 +3,53 @@ using System.Collections;
 
 namespace BloodOfEvil.Extensions
 {
-    /// <summary>
-    /// Cette classe vient de Manzalab.
-    /// </summary>
-	public static class ExtensionsLayerMask
+	public static class LayerMaskExtension
 	{
-		public static bool IsInLayerMask(this LayerMask mask, int layer)
-		{
-			return mask.value.IsInMask(layer);
-		}
+	    /// <summary>
+	    /// Permet de créer un layerMask avec des noms de layers pour dire sur quel layer colisionnera un objet, un raycast ou autre.
+	    /// </summary>
+	    public static LayerMask SetColisionLayerMask(this LayerMask layerMask, params string[] colisionLayerNames)
+	    {
+			layerMask = 0;
 
-		public static bool IsInLayerMask(this LayerMask mask, Collider collider)
-		{
-			return collider != null && collider.gameObject != null && mask.value.IsInMask(collider.gameObject.layer);
-		}
+			foreach (string colisionLayerName in colisionLayerNames)
+				layerMask = layerMask.AddColisionLayerMask(colisionLayerName);
 
-		public static bool IsInMask(this int mask, int layer)
-		{
-			return (mask & (1 << layer)) > 0;
-		}
+			return layerMask;
+	    }
+
+	    /// <summary>
+	    /// Permet de rajouter un layer de colision a un layerMask.
+	    /// </summary>
+	    public static LayerMask AddColisionLayerMask(this LayerMask layerMask, string colisionLayerName)
+	    {
+			return layerMask |= LayerNameIndexToBitShiftValue(colisionLayerName);
+	    }
+
+	    /// <summary>
+	    /// Permet d'enlever un layer de colision a un layerMask.
+	    /// </summary>
+	    public static LayerMask RemoveColisionLayerMask(this LayerMask layerMask, string colisionLayerName)
+	    {
+			return layerMask &= ~(LayerNameIndexToBitShiftValue(colisionLayerName));
+	    }
+
+	    /// <summary>
+	    /// Permet de déterminer si un layer de colision est actif ou non.
+	    /// </summary>
+	    public static bool TestColisionLayerMask(this LayerMask layerMask, string colisionLayerName)
+	    {
+			return (layerMask & LayerNameIndexToBitShiftValue(colisionLayerName)) != 0;
+	    }
+
+	    /// <summary>
+	    /// Exemple : 
+	    /// - LayerMask.NameToLayer("ClickableObject") = 8, il semblerait que cela soit l'index du layer et non sa valeur en décallage binaire.
+	    /// - LayerNameIndexToBitShiftValue("ClickableObject") = 256.
+	    /// </summary>
+	    public static int LayerNameIndexToBitShiftValue(string colisionLayerName)
+	    {
+			return (1 << LayerMask.NameToLayer(colisionLayerName));
+	    }
 	}
 }
