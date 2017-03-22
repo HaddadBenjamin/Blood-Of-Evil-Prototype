7dfb55740251f575cd0c4a961c0d4a5f21dddf9a
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-03-22 10:50:58 +0100

    Update CanvasGroupFade.cs

--- Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/UI/Canvas Fade/CanvasGroupFade.cs
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/UI/Canvas Fade/CanvasGroupFade.cs
@@ -5,21 +5,74 @@ using System.Collections;
 public class CanvasGroupFade : MonoBehaviour
 {
     #region Fields
+    /// <summary>
+    /// C'est le canvas group de l'objet actuel, il permet de gérer la transparence du widget ainsi que ceux de ses enfants.
+    /// </summary>
     private CanvasGroup canvasGroup;
-    [SerializeField]
-    public float fadeSpeed = 1.0f;
+    [SerializeField, Tooltip("C'est la vitesse de fade in fade out.")]
+    private float fadeSpeed = 1.0f;
 
+    /// <summary>
+    /// C'est le mode de fade, il peut être en fade in, en fade out, ou rien du tout.
+    /// </summary>
     private EFadeMode fadeMode;
+
+    /// <summary>
+    /// C'est l'évênement qui est appelé lorsqu'un effet de fade in vient de se terminer.
+    /// </summary>
+    public Action OnFadeInFinish;
+    /// <summary>
+    /// C'est l'évênement qui est appelé lorsqu'un effet de fade out vient de se terminer.
+    /// </summary>
+    public Action OnFadeOutFinish;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Permet de récupérer la valeur de transparence du widget.
+    /// </summary>
+    public float Alpha
+    {
+        get { return this.canvasGroup.alpha; }
+    }
+
+    /// <summary>
+    /// Permet de récupérer le mode de fade courant.
+    /// </summary>
+    public EFadeMode Mode
+    {
+        get { return this.fadeMode; }
+    }
     #endregion
 
     #region Unity Behaviour
     private void Awake()
     {
+        /// Récupère le composant permettant de gérer la transpanrence du widget.
         this.canvasGroup = GetComponent<CanvasGroup>();
     }
     #endregion
 
     #region Public Behaviour
+    /// <summary>
+    /// Modifie la valeur de transpanrence du widget.
+    /// </summary>
+    public void SetAlpha(float alpha)
+    {
+        this.canvasGroup.alpha = alpha;
+    }
+
+    /// <summary>
+    /// Modifie la vitesse de fade in fade out.
+    /// </summary>
+    public void SetSpeed(float speed)
+    {
+        this.fadeSpeed = speed;
+    }
+
+    /// <summary>
+    /// Lance un effet de fade in.
+    /// </summary>
     public void FadeIn()
     {
         this.fadeMode = EFadeMode.FadeIn;
@@ -27,6 +80,9 @@ public class CanvasGroupFade : MonoBehaviour
         StartCoroutine(this.FadeInCoroutine());
     }
 
+    /// <summary>
+    /// Lance un effet de fade out.
+    /// </summary>
     public void FadeOut()
     {
         this.fadeMode = EFadeMode.FadeOut;
@@ -34,6 +90,9 @@ public class CanvasGroupFade : MonoBehaviour
         StartCoroutine(this.FadeOutCoroutine());
     }
 
+    /// <summary>
+    /// Arrête l'effet de fade actuel.
+    /// </summary>
     public void Stop()
     {
         this.fadeMode = EFadeMode.NoFade;
@@ -47,28 +106,32 @@ public class CanvasGroupFade : MonoBehaviour
 
         this.canvasGroup.interactable = true;
 
-        while (this.canvasGroup.alpha < 1.0f &&
-               EFadeMode.FadeOut == this.fadeMode)
-        {
-            this.canvasGroup.alpha += Time.deltaTime * this.fadeSpeed;
-
-            yield return new WaitForEndOfFrame();
-        }
-    }
-
-    private IEnumerator FadeInCoroutine()
-    {
-        yield return null;
-
         while (this.canvasGroup.alpha > 0.0f &&
-               EFadeMode.FadeIn == this.fadeMode)
+               EFadeMode.FadeOut == this.fadeMode)
         {
             this.canvasGroup.alpha -= Time.deltaTime * this.fadeSpeed;
 
             yield return new WaitForEndOfFrame();
         }
 
+        this.OnFadeOutFinish.SafeCall();
+    }
+
+    private IEnumerator FadeInCoroutine()
+    {
+        yield return null;
+
+        while (this.canvasGroup.alpha < 1.0f &&
+               EFadeMode.FadeIn == this.fadeMode)
+        {
+            this.canvasGroup.alpha += Time.deltaTime * this.fadeSpeed;
+
+            yield return new WaitForEndOfFrame();
+        }
+
         this.canvasGroup.interactable = false;
+
+        this.OnFadeInFinish.SafeCall();
     }
     #endregion
 }
