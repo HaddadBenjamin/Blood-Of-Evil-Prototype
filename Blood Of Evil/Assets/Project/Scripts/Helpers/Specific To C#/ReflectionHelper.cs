b7e3d2b78361cc87dd0dabb8fe45f86a673db590
Benjamin Haddad <haddad.b.pro@gmail.com>
2017-03-26 10:43:32 +0200

    + Méthodes permettant de récupérer tous les types et les instances d'une DLL.

--- Blood Of Evil/Assets/Project/Scripts/Helpers/Specific To C#/ReflectionHelper.cs
+++ Blood Of Evil/Assets/Project/Scripts/Helpers/Specific To C#/ReflectionHelper.cs
@@ -1,7 +1,31 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using BloodOfEvil.Extensions;
+using System.Collections.Generic;
 
 namespace BloodOfEvil.Helpers
 {
+    /// <summary>
+    /// C'est la classe de base contenant les données que l'on a besoin pour utiliser la reflection.
+    /// </summary>
+    public class TypeAndInstanceType
+    {
+        /// <summary>
+        /// Le type de l'objet.
+        /// </summary>
+        public Type Type;
+        /// <summary>
+        /// L'instance de l'objet.
+        /// </summary>
+        public object InstanceType;
+
+        public TypeAndInstanceType(Type type, object instance)
+        {
+            this.Type = type;
+            this.InstanceType = instance;
+        }
+    }
+
     public static class ReflectionHelper
     {
         /// <summary>
@@ -14,5 +38,23 @@ namespace BloodOfEvil.Helpers
                    BindingFlags.Static |
                    BindingFlags.Public;
         }
-    }
+
+        /// <summary>
+        /// Récupère tous les types d'une librairie spécifique ou de toutes les librairies trouvées.
+        /// </summary>
+        public static TypeAndInstanceType[] GetAllTypesAndInstanceTypesFromALibrary(string libraryName)
+        {
+            Module[] allModules = AppDomain.CurrentDomain.Load(libraryName).GetModules();
+            List<TypeAndInstanceType> typeAndInstanceTypes = new List<TypeAndInstanceType>();
+            
+            foreach (var module in allModules)
+            {
+                var allTypes = module.GetTypes();
+
+                foreach (var type in allTypes)
+                    typeAndInstanceTypes.Add(new TypeAndInstanceType(type, type.TypeToInstance()));
+            }
+
+            return typeAndInstanceTypes.ToArray();
+        }
 }
