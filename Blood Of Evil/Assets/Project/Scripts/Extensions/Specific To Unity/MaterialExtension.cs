00449caa213b81e0588e555c7311461b65f3f812
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-04-19 10:02:59 +0200

    Create MaterialExtension.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/MaterialExtension.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+namspace BloodOfEvil
+{
+    public static class MaterialExtension
+    {
+        /// <summary>
+        /// Permet de modifier la valeur d'intensité de la couleur d'émission.
+        /// Exemple d'utilisation : 
+        /// this.shaderMaterial = GetComponent<MeshRenderer>().materials[0];
+        /// this.defaultEmissionColor = this.shaderMaterial.GetColor("_EmissionColor");
+        /// this.shaderMaterial.ModifyEmissionScale(this.defaultEmissionColor, this.emissionColorIntensity);
+        /// </summary>
+        public static void ModifyEmissionScale(this Material material, Color defaultEmissionColor, float intensity = 1.0f)
+        {
+            material.EnableKeyword("_EMISSION");
+
+            material.SetColor("_EmissionColor", defaultEmissionColor * intensity);
+        }
+    }
+}
