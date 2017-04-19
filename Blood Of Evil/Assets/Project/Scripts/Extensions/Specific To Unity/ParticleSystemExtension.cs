36daed96c89eca6cdd10c10e73644b15f3b0fd9f
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-04-19 10:05:26 +0200

    Update ParticleSystemExtension.cs

--- Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/ParticleSystemExtension.cs
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/ParticleSystemExtension.cs
@@ -5,11 +5,40 @@ namespace BloodOfEvil.Extensions
 {
     public static class ParticleSystemExtension
     {
-      public static void SafePlay(this ParticleSystem particleSystem)
-      {
-          if (null != particleSystem && 
-              false == particleSystem.isPlaying)
-              particleSystem.Play();
-      }
+      /// <summary>
+    /// Permet de jouer un système de particule de façon sécurisé et permet de le jouer plusieurs fois.
+    /// </summary>
+    public static void SafePlay(
+        this ParticleSystem particleSystem, 
+        bool canSpamThisParticleSystem = false)
+    {
+        if (canSpamThisParticleSystem)
+        {
+            particleSystem.SafeStop();
+            particleSystem.SafeClear();
+        }
+
+        if (null != particleSystem && 
+            false == particleSystem.isPlaying)
+            particleSystem.Play();
+    }
+
+    /// <summary>
+    /// Permet de vider le système de particule en ne risquant pas de crashs.
+    /// </summary>
+    public static void SafeClear(this ParticleSystem particleSystem)
+    {
+        if (null != particleSystem)
+            particleSystem.Clear();
+    }
+
+    /// <summary>
+    /// Permet d'arrêter le système de particule en ne risquant pas de crashs.
+    /// </summary>
+    public static void SafeStop(this ParticleSystem particleSystem)
+    {
+        if (null != particleSystem)
+            particleSystem.Stop();
+    }
     }
 }
