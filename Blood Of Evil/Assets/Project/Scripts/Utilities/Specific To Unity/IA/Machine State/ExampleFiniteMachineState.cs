78f46e10f847a9da9067e0bc6e820ae92fbb26b7
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-04-26 11:51:45 +0200

    Create ExampleFiniteMachineState.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/IA/Machine State/ExampleFiniteMachineState.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BloodOfEvil.Utilities
+{
+    /// <summary>
+    /// Voici un example d'une énumération représentant l'identifiant d'un état.
+    /// </summary>
+    public enum EExampleState
+    {
+        Idle,
+        Wander,
+    }
+
+    /// <summary>
+    /// Ce script montre comment on peut créer un état.
+    /// </summary>
+    public class ExampleIdleState : AState<ExampleFiniteMachineState, EExampleState>
+    {
+        public ExampleIdleState(
+            ExampleFiniteMachineState machineState,
+            EExampleState enumerationState) :
+            base(machineState, enumerationState)
+        {
+        }
+    }
+    public class ExampleWanderState : AState<ExampleFiniteMachineState, EExampleState>
+    {
+        public ExampleWanderState(
+            ExampleFiniteMachineState machineState,
+            EExampleState enumerationState) :
+            base(machineState, enumerationState)
+        {
+        }
+    }
+
+    public class ExampleFiniteMachineState : AFiniteMachineState<ExampleFiniteMachineState, EExampleState>
+    {
+        protected override EExampleState GetDefaultStateID()
+        {
+            return EExampleState.Idle;
+        }
+
+        protected override void CreateStates()
+        {
+            base.states = new AState<ExampleFiniteMachineState, EExampleState>[]
+            {
+                new ExampleIdleState(this, EExampleState.Idle),
+                new ExampleWanderState(this, EExampleState.Wander)
+            };
+        }
+
+        protected override void UpdateState()
+        {
+        }
+    }
+}
