598ccdcc3c56cabb89fe78ebde7490e209a9c8c3
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-05-13 17:03:48 +0200

    Create ButtonExtension.cs

---
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/ButtonExtension.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+namespace BloodOfEvil.Extension
+{
+    using Helpers;
+    
+    public static class ButtonExtension
+    {
+        public static void AddResponsiveOnClickListener(this Button button, Action listener)
+        {
+            button.ForceGetEventTrigger().
+                AddUIListener(UIHelper.GetCrossPlatformOnClickEventTriggerType(), (data) => listener.SafeCall());
+        }
+
+        public static void AddResponsiveOnClickListener(this Button button, Action<BaseEventData> listener)
+        {
+            button.ForceGetEventTrigger().
+                GetComponent<EventTrigger>().AddUIListener(UIHelper.GetCrossPlatformOnClickEventTriggerType(), (data) => listener.SafeCall(data));
+        }
+
+        public static EventTrigger ForceGetEventTrigger(this Button button)
+        {
+            EventTrigger eventTrigger = button.GetComponent<EventTrigger>();
+
+            if (null == eventTrigger)
+                eventTrigger = button.gameObject.AddComponent<EventTrigger>();
+
+            return eventTrigger;
+        }
+    }
+}
