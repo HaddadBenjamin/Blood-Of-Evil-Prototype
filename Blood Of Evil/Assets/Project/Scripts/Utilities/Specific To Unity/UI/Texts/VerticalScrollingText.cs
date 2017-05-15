f85f4ffbce940620583b1b0125b2909096fc8dd5
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-05-15 15:18:52 +0200

    Update VerticalScrollingText.cs

--- Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/UI/Texts/VerticalScrollingText.cs
+++ Blood Of Evil/Assets/Project/Scripts/Utilities/Specific To Unity/UI/Texts/VerticalScrollingText.cs
@@ -10,6 +10,9 @@ namespace BloodOfEvil.Utilities
         private RectTransform rectTransform;
         [SerializeField]
         private float scrolingSpeed = 50.0f;
+        [SerializeField]
+        private float idleTimer = 1.5f;
+        private float idleTime = 0.0f;
 
         private void Awake()
         {
@@ -18,17 +21,25 @@ namespace BloodOfEvil.Utilities
 
         private void FixedUpdate()
         {
-            this.Scroll();
+            this.idleTime += Time.deltaTime;
+
+            if (this.idleTime >= this.idleTimer)
+                this.Scroll();
         }
 
         public void Reset()
         {
+            this.idleTime = 0.0f;
+
             this.rectTransform.SetLocalPositionY(0.0f);
         }
 
         private void Scroll()
         {
             this.rectTransform.SetLocalPositionY(Time.deltaTime * this.scrolingSpeed, true);
+
+            if (this.rectTransform.localPosition.y > Mathf.Abs(this.rectTransform.GetHeight()))
+                this.Reset();
         }
     }
 }
