14709a7841e312ff29b087ac468a66063de741ec
Un passionné dans la foule <haddad.b.pro@gmail.com>
2017-09-07 09:18:29 +0200

    Update AudioClipExtension.cs

--- Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/AudioClipExtension.cs
+++ Blood Of Evil/Assets/Project/Scripts/Extensions/Specific To Unity/AudioClipExtension.cs
@@ -21,33 +21,5 @@ namespace BloodOfEvil.Extensions
                 audioSource.Play();
             }
         }
-
-        //////////////////////////////////////////////
-
-        /// <summary>
-        /// Cette méthode vient de Manzalab.
-        /// Convertit un audio clip en tableau d'octets.
-        /// (utilisation directe des samples --> IeeeFloat wave)
-        /// </summary>
-        /// <returns>Un tableau d'octeToBytests</returns>
-        public static byte[] ToBytes(this AudioClip ac)
-        {
-            byte[] sampleBytes;
-            byte[] bytes = new byte[ac.samples * 4];
-            float[] samples = new float[ac.samples];
-
-            ac.GetData(samples, 0);
-
-            int offset = 0;
-            for (int i = 0; i < ac.samples; i++)
-            {
-                sampleBytes = BitConverter.GetBytes(samples[i]);
-
-                for (int b = 0; b < sampleBytes.Length; b++, offset++)
-                    bytes[offset] = sampleBytes[b];
-            }
-
-            return bytes;
-        }
     }
 }
